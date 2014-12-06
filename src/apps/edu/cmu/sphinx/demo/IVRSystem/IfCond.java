65f981c3c59a7dcfbcaa5915ebd1f13854d0a021
hossam <hossam693@gmail.com>
2014-12-06 16:27:46 +0200

    updated IfCond and Field

--- src/apps/edu/cmu/sphinx/demo/IVRSystem/IfCond.java
+++ src/apps/edu/cmu/sphinx/demo/IVRSystem/IfCond.java
@@ -5,9 +5,17 @@ import java.util.ArrayList;
 public class IfCond {
 	String cond;
 	ArrayList<String> clearList;
+	boolean isCond;
+	String text;
 
 	public IfCond(String cond, ArrayList<String> clearList) {
 		this.cond = cond;
 		this.clearList = clearList;
+		isCond = true;
+	}
+
+	public IfCond(String text) {
+		this.text = text;
+		isCond = false;
 	}
 }
